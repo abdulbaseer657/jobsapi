ce4977b95adc7d85733e9814672fbc977d6c7306
Mohammed Abdul Baseer <52657917+abdulbaseer657@users.noreply.github.com>
2022-11-21 21:47:02 -0600

    Update curd.js

--- controllers/curd.js
+++ controllers/curd.js
@@ -10,6 +10,11 @@ const jobs = require("../models/jobs");
 // };
 
 const getalljobs = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   const { companyname, internship, jobid, domain, jobtitle, sort, fields } =
     req.query;
   const queryObject = {};
