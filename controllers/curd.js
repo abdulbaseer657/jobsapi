ba13833a047f9777c88df9844aa67830a7f7a8b2
Mohammed Abdul Baseer <52657917+abdulbaseer657@users.noreply.github.com>
2022-11-22 13:25:13 -0600

    Update curd.js

--- controllers/curd.js
+++ controllers/curd.js
@@ -11,10 +11,8 @@ require("cors");
 // };
 
 const getalljobs = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin ", "http://localhost:3006/");
+  res.setHeader("Access-Control-Allow-Origin ", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
