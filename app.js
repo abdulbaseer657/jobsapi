03dc46cef1a92aee72279479c9f6194598dab850
Mohammed Abdul Baseer <52657917+abdulbaseer657@users.noreply.github.com>
2022-11-22 13:31:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var cors = require('cors');
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(function(req,res,next){
+        res.setHeader("Access-Control-Allow-Origin ", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  next();
+})
 
 //routes
 app.use("/api/v1/", jobs);
